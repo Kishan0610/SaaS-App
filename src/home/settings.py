e1a2809cc6b874ea0c1bb8812efb8a71b58bdd99
kishan0610 <061002kishan@gmail.com>
2024-07-01 23:37:56 +0530

    Prepare for railway

--- src/home/settings.py
+++ src/home/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c+wnp58ga0l$^h&47t*@=&^@p1*w%#p#x8p**qojd($p-k@05o"
+SECRET_KEY = "1$fwbd_17v#)pd2yz)vakf6s+n6fr#p$zgw@f)y(_kd*500ggk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas.prod.railway.app
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = [
 if DEBUG:
     ALLOWED_HOSTS += [
         "127.0.0.1",
-        "localhost"
+        "localhost",
+        "Saas-Application"
     ]
 
 
--- src/home/views.py
+++ src/home/views.py
@@ -28,19 +28,20 @@ def about_view(request, *args, **kwargs):
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
-def old_home_page_view(request, *args, **kwargs):
+def my_old_home_page_view(request, *args, **kwargs):
     my_title = "My Page"
     my_context = {
         "page_title": my_title
     }
     html_ = """
     <!DOCTYPE html>
-        <html>
-        <title></title>
-        <body>
-            <h1>Is this Anything?</h1>
-        </body>
-        </html>
+    <html>
 
-    """.format(**my_context)
+    <body>
+        <h1>{page_title} anything?</h1>
+    </body>
+    </html>    
+    """.format(**my_context) # page_title=my_title
+        # html_file_path = this_dir / "home.html"
+        # html_ = html_file_path.read_text()
     return HttpResponse(html_)
