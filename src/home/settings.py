a92bb6ebca06957e5194a8a28647b4ad0eeb2db8
kishan0610 <061002kishan@gmail.com>
2024-07-01 23:01:10 +0530

    Updated Views

--- src/home/settings.py
+++ src/home/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = "django-insecure-c+wnp58ga0l$^h&47t*@=&^@p1*w%#p#x8p**qojd($p-k@05o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" #https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
--- src/home/urls.py
+++ src/home/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home_page_view, old_home_page_view
+from .views import home_view, about_view
 
 urlpatterns = [
-    path("", home_page_view), # index Page
-    path("hello-world", old_home_page_view),
+    path("", home_view), # index Page
+    path("about/", about_view),
+    path("hello-world/", home_view),
+    path("hello-world.html/", home_view),
     path("admin/", admin.site.urls),
 ]
