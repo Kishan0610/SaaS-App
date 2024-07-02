c1142837dfe949d3e72b738b39b9e7529817bba8
kishan0610 <061002kishan@gmail.com>
2024-07-02 18:34:21 +0530

    Django Debug envoron var

--- src/home/settings.py
+++ src/home/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-c+wnp58ga0l$^h&47t*@=&^@p1*w%#p#x8p**qojd($p-k@05o"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("Django_DEBUG")).lower() == "true"
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas.prod.railway.app
